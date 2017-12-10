4e5cd9257ee40fd6c09db3a10fa4d469b3c90cbf
Jon Craton <jncraton@gmail.com>
2017-12-10 18:24:16 -0500

    Split out constants

---
+++ proceduralbricks/constants.py
@@ -0,0 +1,35 @@
+from enum import Enum
+
+class FillType(Enum):
+  EVEN = 0
+  ODD = 1
+  SOLID = 2
+
+class Facing(Enum):
+  FRONT = (0, 1, 2, 1)
+  BACK = (0, 1, 2, -1)
+  LEFT = (2, 1, 0, 1)
+  RIGHT = (2, 1, 0, -1)
+  def __init__(self, x, y, z, flip):
+    self.x = x
+    self.y = y
+    self.z = z
+    self.flip = flip
+    
+class Parts(Enum):
+  BRICK_1X1 = '3005'
+  BRICK_1X2 = '3004'
+  BRICK_1X2_MASONRY = '98283'
+  WINDOW_1X2X2 = '60592C01'
+  WINDOW_1X2X3 = '60593C01'
+  GLASS_1X4X6 = '57895'
+  DOOR_1X4X6_FRAME = '30179'
+  DOOR_1X4X6_3_PANE = '30178C01'
+  DOOR_1X4X6_4_PANE = '60623'
+
+class Colors(Enum):
+  BLACK = 0
+  TAN = 19
+  SAND_BLUE = 379
+  TRANS_CLEAR = 47
+
