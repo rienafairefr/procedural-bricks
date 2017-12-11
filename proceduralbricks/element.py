a1ef02e293c58c10526e7849e13a92113c461a13
Jon Craton <jncraton@gmail.com>
2017-12-10 21:56:57 -0500

    Added ElementGroup

--- proceduralbricks/element.py
+++ proceduralbricks/element.py
@@ -10,18 +10,14 @@ class Connections:
     self.back = back
 
 class Element:
-  def __init__(self, pos, facing, pos_b=None, connections=Connections(), color=None, part=None):
+  def __init__(self, pos, facing=Facing.FRONT, part='3004', color=0, pos_b=None, connections=Connections()):
     self.pos = pos
-    self.pos_b = pos_b
     self.facing = facing
     self.connections = connections
     self.part = part
     self.color = color
-    self.children = []
 
   def to_ldr(self):
-    print(self.part)
-  
     if self.facing == Facing.LEFT:
       orientation = "0 0 1 0 1 0 -1 0 0"
     if self.facing == Facing.BACK:
@@ -31,9 +27,15 @@ class Element:
     if self.facing == Facing.RIGHT:
       orientation = "-0 0 -1 0 1 0 1 0 0"
 
-    ldr = '\n'.join([c.to_ldr() for c in self.children])
+    return("1 %d %d %d %d %s %s.dat" % (self.color, self.pos[0], self.pos[1], self.pos[2], orientation, self.part))
 
-    if self.part:
-      ldr += "\n1 %d %d %d %d %s %s.dat" % (self.color, self.pos[0], self.pos[1], self.pos[2], orientation, self.part)
+class ElementGroup:
+  def __init__(self, pos, facing, pos_b=None, connections=Connections()):
+    self.pos = pos
+    self.pos_b = pos_b
+    self.facing = facing
+    self.connections = connections
+    self.children = []
 
-    return ldr    
+  def to_ldr(self):
+    return('\n'.join([c.to_ldr() for c in self.children]))
